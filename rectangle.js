73db82bcfa1f8ffad29e535d8bab61a3559b1a42
PeiYumeng <457492875@qq.com>
2020-02-19 11:34:05 +0800

    message

---
+++ rectangle.js
@@ -0,0 +1,22 @@
+$(function(){
+    //get dom elem
+    var $width = $('#width'),
+          $height = $('#height'),
+          $btnCal = $('#calculate'),
+          $perimeter = $('#perimeter'),
+          $area = $('#area');
+    
+    /*calc button click event*/
+    $btnCal.click(function(){
+        //get value
+        var w = Number($width.val()),
+            h = Number($height.val());
+             
+        //calculate
+        var p = Math.round(2*(w+h)*Math.pow(10,4))/Math.pow(10,4);
+            a = Math.round(w*h*Math.pow(10,4))/Math.pow(10,4);
+        //output
+        $perimeter.val(p);
+        $area.val(a)
+    });
+});
